ff898f08ebf341514169542ee195a168414d7fb9
eldaramirtemirov-commits <eldaramirtemirov@gmail.com>
2026-06-09 19:18:16 +0600

    Update index.js

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ const server = http.createServer((req, res) => {
                 await sendTelegramMessage(message);
                 
                 // Простая и надежная склейка ссылки без косых кавычек
-                const testUrl = "https://lava.ru/" + price + "&text=Donate_Minecraft";
+                const testUrl = "https://lava.ru/" + (typeof price !== 'undefined' ? price : testPrice) + "&text=Donate_Minecraft";
                 
                 res.writeHead(200, { 'Content-Type': 'application/json' });
                 res.end(JSON.stringify({ url: testUrl })); 
